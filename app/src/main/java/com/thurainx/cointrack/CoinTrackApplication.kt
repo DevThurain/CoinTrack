60384e430c12d313faee3517618ed610f63c6e4b
thurain <thurainbowin.dev@gmail.com>
2022-03-13 22:21:47 +0630

    - init project

---
+++ app/src/main/java/com/thurainx/cointrack/CoinTrackApplication.kt
@@ -0,0 +1,8 @@
+package com.thurainx.cointrack
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CoinTrackApplication : Application() {
+}
---
+++ app/src/main/java/com/thurainx/cointrack/other/Constant.kt
@@ -0,0 +1,4 @@
+package com.thurainx.cointrack.other
+
+object Constant {
+}
